d1624d32e6b58ed212f21f4309860ad7017b17a1
fschulze <fschulze@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-07 12:40:51 +0000

    Added some sanity checks for the virtualenvs. This shows that python-2.5 is currently broken on Snow Leopard.

---
+++ src/test-python.py
@@ -0,0 +1,12 @@
+def test(options, buildout):
+    from subprocess import Popen, PIPE
+    import os
+    import sys
+
+    python = options['python']
+    if not os.path.exists(python):
+        raise IOError("There is no file at %s" % python)
+    if sys.platform == 'darwin':
+        output = Popen([python, "-c", "import platform; print platform.mac_ver()"], stdout=PIPE).communicate()[0]
+        if not output.startswith("('10."):
+            raise IOError("Your python at %s doesn't return proper data for platform.mac_ver(), got: %s" % (python, output))
