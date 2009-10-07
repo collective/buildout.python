42081679c9ac7d064b2ca1ec46e6bd64a57fcaf2
fschulze <fschulze@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-07 11:37:19 +0000

    Platform auto detection, no need for a snowleopard config anymore, should also work on linux now.

---
+++ src/osdetect.py
@@ -0,0 +1,24 @@
+def osdetect(buildout):
+    import sys, platform
+
+    platforms = ['default']
+    if sys.platform == 'darwin':
+        platforms.insert(0, 'darwin')
+        mac_ver = platform.mac_ver()
+        if mac_ver[0].startswith('10.5'):
+            platforms.insert(0, 'darwin-leopard')
+        elif mac_ver[0].startswith('10.6'):
+            platforms.insert(0, 'darwin-snowleopard')
+
+    variants = {}
+    for key in buildout.keys():
+        if ':' not in key:
+            continue
+        part, variant = key.split(':')
+        variants.setdefault(variant, []).append((part, key))
+
+    for platform in platforms:
+        for part, key in variants.get(platform, []):
+            if part in buildout:
+                continue
+            buildout._raw[part] = buildout._raw[key].copy()
