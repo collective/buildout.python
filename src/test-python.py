b039f444f0f8fd7cfce3bfa3ee270ed95562a6f6
fschulze <fschulze@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-06 10:52:14 +0000

    Fix for Python 3.x.

--- src/test-python.py
+++ src/test-python.py
@@ -7,6 +7,6 @@ def test(options, buildout):
     if not os.path.exists(python):
         raise IOError("There is no file at %s" % python)
     if sys.platform == 'darwin':
-        output = Popen([python, "-c", "import platform; print platform.mac_ver()"], stdout=PIPE).communicate()[0]
+        output = Popen([python, "-c", "import platform; print (platform.mac_ver())"], stdout=PIPE).communicate()[0]
         if not output.startswith("('10."):
             raise IOError("Your python at %s doesn't return proper data for platform.mac_ver(), got: %s" % (python, output))
