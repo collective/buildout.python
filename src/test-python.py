5b71b9e86dc09fe21717a75e45748a81d833c632
Juan A. Diaz <juan@linux.org.ar>
2013-11-27 01:18:51 -0600

    Check if the installed python2.4 have ssl support.

--- src/test-python.py
+++ src/test-python.py
@@ -10,3 +10,7 @@ def test(options, buildout):
         output = Popen([python, "-c", "import platform; print (platform.mac_ver())"], stdout=PIPE).communicate()[0]
         if not output.startswith("('10."):
             raise IOError("Your python at %s doesn't return proper data for platform.mac_ver(), got: %s" % (python, output))
+    elif sys.platform == 'linux2' and (2, 4) <= sys.version_info < (2, 5):
+        output = Popen([python, "-c", "import socket; print (hasattr(socket, 'ssl'))"], stdout=PIPE).communicate()[0]
+        if not output.startswith("True"):
+            raise IOError("Your python at %s doesn't have ssl support, got: %s" % (python, output))
