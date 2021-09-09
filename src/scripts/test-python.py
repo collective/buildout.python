069b7765303b41694ba26677feb75cd08bbcf43f
Martin Peeters <martin.peeters@affinitic.be>
2021-09-09 18:18:18 +0200

    Adapt tests for MacOS 11 Big Sur

--- src/scripts/test-python.py
+++ src/scripts/test-python.py
@@ -8,7 +8,7 @@ def test(options, buildout):
         raise IOError("There is no file at %s" % python)
     if sys.platform == 'darwin':
         output = Popen([python, "-c", "import platform; print (platform.mac_ver())"], stdout=PIPE).communicate()[0]
-        if not output.startswith("('10."):
+        if not output.startswith(("('10.", "('11.")):
             raise IOError("Your python at %s doesn't return proper data for platform.mac_ver(), got: %s" % (python, output))
     elif sys.platform == 'linux2' and (2, 4) <= sys.version_info < (2, 5):
         output = Popen([python, "-c", "import socket; print (hasattr(socket, 'ssl'))"], stdout=PIPE).communicate()[0]
